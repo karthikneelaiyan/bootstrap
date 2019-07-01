5d3dbe67e49ca46c84c2084c1fdb69484a139e5d
karthik <karthik.n@prodapt.com>
2019-07-01 11:17:08 +0530

    grunt

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 1000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+
+    $('#reserve_button').on('click', function () {
+        $('#reserve_modal').modal('show');
+    })
+    $('#login_button').on('click', function () {
+        $('#login_modal').modal('show');
+
+    })
+});
